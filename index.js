d4f0deacc24ec092d29d7c56ffcecb4bf518eb8c
Raymond <oetaray@gmail.com>
2023-06-23 10:23:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (feet <= 400){
+  return "This ones on me!"
+}else if(feet >= 2001)
+  return "I will gladly take your thirty bucks."
+  else if (feet >= 2500)
+  return "No can do"
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  if (city ="NYC"){
+  return "Ok, sounds good"
+}else if (city = "Pittsburgh")
+  return "No go"
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+ switch(tip){
+  case 'Generous':
+  return 'Thank you very much!'
+  case 'not as generous':
+  return 'Thank you.'
+  case "Bye":
+  return "Thanks for everything"
+ }
 }
